50826bcb7a809ada068fef9b247e7c18d61f30e5
Joao Porfirio <joaoprf54@gmail.com>
2021-03-22 16:31:38 -0300

    Add function to allow task excludes

--- projeto_inicial/listaDeTarefas.js
+++ projeto_inicial/listaDeTarefas.js
@@ -16,6 +16,7 @@
     tarefa.innerHTML = conteudo;
 
     tarefa.appendChild(BotaoConclui());
+    tarefa.appendChild(BotaoDeleta());
     lista.appendChild(tarefa);
     input.value = "";
 }
@@ -44,7 +45,23 @@ const concluirTarefa = (evento) => {
     tarefaCompleta.classList.toggle('done');
 }
    
+const BotaoDeleta = () =>{
+    const botaoDeleta = document.createElement('button');
+
+    botaoDeleta.innerText = 'deletar';
+    botaoDeleta.addEventListener('click', deletarTarefa)
+
+    return botaoDeleta;
+}
+
+const deletarTarefa = (evento) => {
+    const botaoDeleta = evento.target;
+
+    const tarefaCompleta = botaoDeleta.parentElement;
+    tarefaCompleta.remove();
     
+    return botaoDeleta;
+}
 })();
 
  
